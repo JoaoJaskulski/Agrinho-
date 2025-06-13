7e0b726ff5783aceb8c5f01a584199ea67679baf
JoaoJaskulski <160859383+JoaoJaskulski@users.noreply.github.com>
2025-06-13 08:37:35 -0300

    .

--- main.js
+++ main.js
@@ -6,66 +6,65 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Como faço para conseguir medicamentos não padronizados do sus?",
+        enunciado: "Por que as pessoas migram do campo para a cidade?",
         alternativas: [
             {
-                texto: "encaminhar-se a regional de saude do seu município, munido dos seguintes documentos: cpf ou rg, comprovante de residencia, prescriçao atualizada, receituario medico.",
-                afirmacao: "<b>Organização</b> Sim, a Sesa tem como principal função garantir o acesso ao serviços que visem a promoção, protećão e recuperação da saúde da população em todo o Paraná."
+                texto: "As pessoas migram em busca de oportunidades de trabalho, além de acesso a serviços como educação e saúde, que são mais abundantes nas cidades.",
+                afirmacao: "Pessoa <b>pragmática</b>, buscando melhores condições de vida e oportunidades."
             },
             {
-                texto: "Exigências quanto ao  relatório médico. Os documentos preenchidos pelo médico deverão conter carimbo legível, com nome e CRM do médico, data e assinatura.",
-                afirmacao: "Você se destaca por seu <b>conhecimento</b>, o carimbo deverá conter também o número do Registro de Qualificação de Especialista (RQE)"
+                texto: "Muitas migram por necessidade devido à falta de recursos no campo, como terra fértil ou água. Outras fazem a mudança para realizar sonhos profissionais ou para conquistar maior independência.",
+                afirmacao: "Pessoa <b>realista</b>, migrando por necessidade ou para realizar sonhos."
+        ]
+    },
+    {
+        enunciado: "Quais são os principais desafios dessa migração?",
+        alternativas: [
+            {
+                texto: "Os principais desafios incluem a adaptação ao ritmo acelerado da vida urbana, o alto custo de vida, a dificuldade de encontrar moradia adequada e o desconhecimento da cidade, além de falta de apoio emocional de uma comunidade mais próxima.",
+                afirmacao: "Pessoa <b>pragmática</b>, ciente do alto custo de vida e da adaptação emocional."
+            },
+            {
+                texto: "Também há dificuldades em adaptar as habilidades rurais ao mercado de trabalho urbano. O choque cultural e a solidão são comuns, já que as relações e o estilo de vida no campo são muito diferentes do que se encontra nas grandes cidades.",
+                afirmacao: "Pessoa <b>analítica</b>, reconhecendo dificuldades culturais e no mercado de trabalho."
             }
         ]
     },
     {
-        enunciado: "A falta de consciência ambiental e a baixa adesão a práticas sustentáveis acontecem por vários motivos, entre os quais destacam-se:",
+        enunciado: "Quais são as vantagens da migração para a cidade?",
         alternativas: [
             {
-                texto: "Falta de Educação Ambiental",
-                afirmacao: "Muitas pessoas não têm acesso a informações adequadas sobre o impacto de suas ações no meio ambiente, o que leva à falta de conscientização e, consequentemente, a comportamentos prejudiciais à <b>sustentabilidade</b>.</b>."
+                texto: "A cidade oferece maiores oportunidades de emprego e uma infraestrutura melhor (como transporte, saúde e educação), além de acesso a qualificação profissional, que pode melhorar a vida e as perspectivas de crescimento.",
+                afirmacao: "Pessoa <b>ambiciosa</b>, focada em oportunidades de emprego e qualificação."
             },
             {
-                texto: "Falta de Incentivos e Legislação",
-                afirmacao: "A ausência de incentivos governamentais e de uma legislação <b>rigorosa </b>faz com que empresas e indivíduos não se sintam motivados a adotar práticas sustentáveis, contribuindo para a continuidade de ações nocivas ao meio ambiente."
+                texto: "Além das oportunidades de emprego, a cidade traz uma diversidade cultural rica, com acesso a uma vida social mais dinâmica, novas experiências e, muitas vezes, melhores condições de vida no geral, como acesso a bens e serviços.",
+                afirmacao: "Pessoa <b>curiosa</b>, valorizando diversidade cultural e a vida social."
             }
         ]
     },
     {
-        enunciado: "Por que há tanta escassez de recursos na saúde pública?",
+        enunciado: "A vida na cidade é melhor do que no campo?",
         alternativas: [
             {
-                texto: "Acredito que Falta de investimento governamental",
-                afirmacao: "<b>Pragmatismo</b> A falta de investimento governamental na saúde pública decorre de orçamentos restritos e outras prioridades de gastos, levando a infraestrutura precária e serviços de saúde insuficientes."
-            },
-            {
-                texto: "desigualdades na distribuição de recursos",
-                afirmacao: "<b>Detalhista</b>,Desigualdades na distribuição de recursos na saúde pública ocorrem quando os recursos são alocados de maneira desigual entre diferentes regiões ou populações. Isso pode resultar em acesso desigual a serviços de saúde, com áreas mais pobres ou rurais recebendo menos apoio e, portanto, enfrentando maiores desafios em termos de qualidade e acesso a cuidados médicos."
-            }
-        ]
-    },
-    {
-        enunciado: "Por que a violência obstétrica está cada vez mais banalizada? ",
-        alternativas: [
-            {
-                texto: "Negligência médica",
+                texto: "Pessoa orientada para o futuro, buscando oportunidades profissionais.",
                 afirmacao: "<b>Foco</b> Apesar do uso de algumas técnicas serem restringidas apenas para casos específicos, ainda são utilizadas diariamente por profissionais da saúde."
             },
             {
-                texto: "Desinformação da população.",
+                texto: "No entanto, muitos preferem a vida no campo, que é mais tranquila e próxima da natureza, com menos violência e uma maior sensação de segurança e comunidade, embora a cidade ofereça mais recursos e oportunidades.",
                 afirmacao: "<b>Curiosidade</b> Mesmo com fácil acesso a informação, o assunto é pouco comentado e dado certa  importância. "
             }
         ]
     },
     {
-        enunciado: "Como evitar que ocorra a extinção de animais nativos?",
+        enunciado: "Como a migração impacta a cultura urbana?",
         alternativas: [
             {
-                texto: "Criação de mais áreas de proteção ambiental, com grande biodiversidade ajudaria a melhorar esse cenário?",
+                texto: "A migração traz novas tradições culturais, como festas típicas, culinária e músicas regionais, que enriquecem a vida urbana e a tornam mais diversa e plural.",
                 afirmacao: "Sim, pois preservando nosso meio ambiente, protegemos nossos animais e facilitamos sua reprodução "
             },
             {
-                texto: "Evitar intervenções de espécies não nativas neste ambiente pode ser uma alternativa?",
+                texto: "Além disso, os migrantes do campo podem trazer uma visão mais simples e prática da vida, com valorização do trabalho manual e relações comunitárias, que podem influenciar positivamente a cultura da cidade.",
                 afirmacao: "Sim, animais de um determinado local não estão preparados para lidar com predadores de outras áreas, levando a um extermínio da espécie local"
             }
         ]
